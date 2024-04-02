9c272bf443f1ae1c8a648bac1dd2ea65aca1f333
DESKTOP-747UBGO\Enrique <enriquee.flor@gmail.com>
2024-04-01 21:12:02 -0300

    Add project files.

---
+++ Triangulo_estudo/Program.cs
@@ -0,0 +1,41 @@
+﻿
+using ConsoleApp1;
+using System.Globalization;
+
+
+
+Triangulo x, y;
+
+x = new Triangulo();
+y = new Triangulo();
+
+
+Console.WriteLine("Entre com as medidas do triangulo X:");
+
+x.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+x.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+x.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+
+Console.WriteLine("Entre com as medidas do triangulo Y:");
+
+y.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+y.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+y.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+
+double p = (x.A + x.B + x.C) / 2.0;
+double areaX = Math.Sqrt(p * (p - x.A) * (p - x.B) * (p - x.C));
+
+p = (y.A + y.B + y.C) / 2.0;
+double areaY = Math.Sqrt(p * (p - y.A) * (p - x.B) * (p - x.C));
+
+if (areaX > areaY)
+{
+    Console.WriteLine("X e maior");
+}
+
+else
+{
+    Console.WriteLine("Y e maior");
+}
